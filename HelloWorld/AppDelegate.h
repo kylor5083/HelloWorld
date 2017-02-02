2ee5f0833599c446bf56acc9b02aebbef40e6402
orangeday77 <orangeday77@gmail.com>
2017-02-02 16:17:37 +0900

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by orangeday77 on 2017. 2. 2..
+//  Copyright © 2017년 YongDoKim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by orangeday77 on 2017. 2. 2..
+//  Copyright © 2017년 YongDoKim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
